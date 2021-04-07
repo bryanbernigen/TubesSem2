58a1b34f50cf7983dc8155c197ed9d0aa89c49c2
Bryan Bernigen <bryanbernigen@gmail.com>
2021-04-07 23:08:31 +0700

    Modified read_csv.py Added function to read all csv datas in csv_datas using unique function for each file

---
+++ main.py
@@ -0,0 +1,8 @@
+from read_csv import * #import semua function di read_csv
+
+user=read_user("user.csv")
+gadget=read_gadget("gadget.csv")
+consumbale=read_consumable("consumable.csv")
+consumable_history=read_consumable_history("consumable_history.csv")
+gadget_borrow_history=read_gadget_borrow_history("gadget_borrow_history.csv")
+gadget_return_history=read_gadget_return_history("gadget_return_history.csv")
