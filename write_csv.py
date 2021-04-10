dfef89820a165bd1c5575e7872c0d469773b49a3
Bryan Bernigen <bryanbernigen@gmail.com>
2021-04-10 10:09:22 +0700

    Editted write_csv.py Added function to save files to a folder or overwrite a folder

--- write_csv.py
+++ write_csv.py
@@ -2,10 +2,11 @@
 '''
 ---
 '''
+import os
 
 #Fungsi memerlukan 2 input yaitu data yang akan di save dan tempat data tersebut akan di save
 #Tempat data tersebut di save harus sudah ada (boleh kosong)
-def write_csv(data_yang_akan_di_save,nama_file_yang_di_save):
+def write_csv(data_yang_akan_di_save,nama_file_yang_di_save,path_relatif_terhadap_sekarang):
     data=data_yang_akan_di_save
 
     #menghitung jumlah kolom dan baris dari data yang di read
@@ -36,9 +37,27 @@ def write_csv(data_yang_akan_di_save,nama_file_yang_di_save):
             var_dummy+=list_array[j][i]
     #list_panjang.append(var_dummy)
 
-    f=open("csv_datas/{}".format(nama_file_yang_di_save),"w")
+    f=open("{}/{}".format(path_relatif_terhadap_sekarang,nama_file_yang_di_save),"w")
     f.write(var_dummy)
 
+#save data ke sebuah folder
+#input selain path merupakan sebuah matriks (seperti ini: [["a","b","c"],["d","e","f"]])
+def save(path_relatif_terhadap_sekarang,user,gadget,consumable,consumable_history,gadget_borrow_history,gadget_return_history):
+    #membuat path tempat file akan di save
+    path_sekarang=os.getcwd()
+    new_path=os.path.join(path_sekarang,path_relatif_terhadap_sekarang)
+    
+    #jika path belum ada, maka path akan dibuat
+    if not os.path.exists(new_path):
+        os.mkdir(new_path)
+
+    #Mensave data
+    write_csv(user,"user.csv",new_path)
+    write_csv(gadget,"gadget.csv",new_path)
+    write_csv(consumable,"consumable.csv",new_path)
+    write_csv(consumable_history,"consumable_history.csv",new_path)
+    write_csv(gadget_borrow_history,"gadget_borrow_history.csv",new_path)
+    write_csv(gadget_return_history,"gadget_return_history.csv",new_path)
 
 #Contoh Penggunaan
 '''
