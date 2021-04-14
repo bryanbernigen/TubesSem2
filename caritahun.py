d45c54679ac9001ce0105fa6af6c7bdf0cbff085
Putri Alfiyyahdianti <putrialfiyahd@gmail.com>
2021-04-14 11:44:14 +0700

    added caritahun.py

---
+++ caritahun.py
@@ -0,0 +1,11 @@
+from read_csv import read_gadget
+
+def caritahun(gadget):
+    x=input('Masukkan tahun :')
+    y=input('Masukkan kategori:')
+    print ('hasil pencarian :')
+
+    for i in range (len(gadget)):
+        if y== ">":
+            j= gadget []
+            print (gadget [0] + ':'+)
