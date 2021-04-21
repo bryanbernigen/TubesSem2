d4a74ad2e64230fe8c96576232afdddd85272dd7
Putri Alfiyyahdianti <putrialfiyahd@gmail.com>
2021-04-21 18:01:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -174,26 +174,26 @@ while valid:
     #F16 - Help
     if pilihan == 'help':
         print("============= HELP ============="
-              "\nregister - untuk melakukan registrasi user baru"
-              "\nlogin - untuk melakukan login ke dalam sistem"
-              "\ncarirarity - untuk mencari gadget dengan rarity tertentu"
-              "\ncaritahun - untuk mencari gadget berdasarkan tahun ditemukan"
+              "\nregister              - untuk melakukan registrasi user baru"
+              "\nlogin                 - untuk melakukan login ke dalam sistem"
+              "\ncarirarity            - untuk mencari gadget dengan rarity tertentu"
+              "\ncaritahun             - untuk mencari gadget berdasarkan tahun ditemukan"
               )
         if curret_role=='admin':
-            print("tambahitem - untuk menambahkan item ke dalam inventori"
-                  "\nhapusitem - untuk menghapus suatu item pada database"
-                  "\nubahjumlah - untuk mengubah jumlah gadget dan consumable dalam sistem"
-                  "\nriwayatkembali - untuk melihat riwayat pengembalian gadget"
-                  "\nriwayatambil - untuk melihat riwayat pengambilan consumable"
+            print("tambahitem          - untuk menambahkan item ke dalam inventori"
+                  "\nhapusitem         - untuk menghapus suatu item pada database"
+                  "\nubahjumlah        - untuk mengubah jumlah gadget dan consumable dalam sistem"
+                  "\nriwayatkembali    - untuk melihat riwayat pengembalian gadget"
+                  "\nriwayatambil      - untuk melihat riwayat pengambilan consumable"
                     )
         else:#curret_role=='user'
-            print("pinjam - untuk melakukan peminjaman gadget"
-                  "\nkembalikan - untuk mengembalikan gadget"
-                  "\nminta - untuk meminta consumable yang tersedia"
+            print("pinjam              - untuk melakukan peminjaman gadget"
+                  "\nkembalikan        - untuk mengembalikan gadget"
+                  "\nminta             - untuk meminta consumable yang tersedia"
                     )
-        print("save - untuk melakukan penyimpanan data"
-              "\nhelp - untuk panduan penggunaan penggunaan sistem"
-              "\nexit - untuk keluar dari aplikasi"
+        print("save                    - untuk melakukan penyimpanan data"
+              "\nhelp                  - untuk panduan penggunaan penggunaan sistem"
+              "\nexit                  - untuk keluar dari aplikasi"
                 )
 
     #F17 - Exit
