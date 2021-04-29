41aa696dc275157cb4660e4510476db2e1d8bdc8
Bryan Bernigen <bryanbernigen@gmail.com>
2021-04-29 10:28:28 +0700

    Eddited Hashing Algorithm Hashing become 1 way

--- hashing.py
+++ hashing.py
@@ -18,53 +18,35 @@ def hashing(password,username):
     #Membuat Angka unik dari setiap password dan username
     def unique_number(password_and_username):
         unique_number=0
-        pengali=1
-        for i in range(len(password)):
-            for ii in range (panjang_kamus):
-                if password[i]==kamus[ii]:
-                    unique_number += ii*pengali
-                    pengali*=10
+        for i in range(len(password_and_username)):
+            unique_number+=ord(password_and_username[i])
         return unique_number
-    unique_number=unique_number(password)
+    number=unique_number(password_and_username)
 
     #Menambah Panjang password yang kurang dari 32
     def perpanjang_password(password_and_username, unique_number):
         panjang_kurang=32-len(password_and_username)
         for i in range (panjang_kurang):
-            algorithm_number=(unique_number*i + i)%panjang_kamus 
-            password_and_username+=kamus[algorithm_number]
+            password_and_username+=chr(i*unique_number%256)
         return password_and_username
-    password_32 = perpanjang_password(password_and_username,unique_number)
+    password_32 = perpanjang_password(password_and_username,number)
 
+    #Mengubah Password menjadi bentuk hashed(integer) agar 1 arah
+    def hashed(password_32):
+        hashed=0
+        for j in range(len(password_32)):
+            hashed+=ord(password_32[j])*(10**(j%8)) 
+        return hashed
+    hashed_pw=hashed(password_32)
 
-    #Mengubah Password menjadi Array
-    def array_password(password):
-        array_password=[]
-        for i in range(len(password)):
-            array_password.append(password[i])
-        return array_password
-    array_password=array_password(password_32)
-
-    #Menghashed Password
-    def hashed(array_password,unique_number):
-        for i in range(len(array_password)):
-            for ii in range(panjang_kamus):
-                if array_password[i]==kamus[ii]:
-                    algorithm_number=(unique_number*i + unique_number*ii + unique_number*i*ii + i*ii + i + ii + unique_number)%panjang_kamus
-                    array_password[i]=kamus[algorithm_number]
-        return array_password
-    password_hashed=hashed(array_password,unique_number)
-
-    #Mengembalikan Hasil Hashed dalam array ke bentuk string
-    def password_final(password_hashed):
-        password_final=""
-        for i in range(len(password_hashed)):
-            password_final+=password_hashed[i]
-        return password_final
-    password_final=password_final(password_hashed)
+    #jika hasil hashed lebih dari 99999999, maka akan di floor agar menjadi kurang dari 99999999
+    def final_stage(hashed_pw):
+        while len(str(hashed_pw))>8:
+            hashed_pw=hashed_pw//10
+        return str(hashed_pw)
+    password_final=final_stage(hashed_pw)
 
     return password_final
-
 
 #Contoh Pengaplikasian
 '''
